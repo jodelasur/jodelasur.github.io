e3ea9e66c219b216f6e2de7a97a91825b8327fc2
Jodel Asur <jodelasur@gmail.com>
2018-06-26 06:04:01 +0000

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,21 @@
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+    
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            mainImage.setAttribute("src", newImageSrc);
+            
+            const currentClass = "current";
+            document.querySelector(".current").classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+            
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
 }
